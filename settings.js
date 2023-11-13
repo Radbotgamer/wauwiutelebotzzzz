f8413385c24cd37648bf10798cda07325be180e8
Radbotgamer <98032766+Radbotgamer@users.noreply.github.com>
2023-11-13 18:51:19 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const {
+   indonesia
+} = require("./language");
+
+// Website Api (jgn di ganti biar gk eror)
+global.APIs = {
+   alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
+}
+
+//buy apikey premium 0887435047326
+// Free apikey (silahkan login terus ganti Your Key dgn apikey lu)
+global.APIKeys = {
+   'https://api.zeeoneofc.my.id': 'pp4IAhwNa4VI97k', // 👉 login https://api.zeeoneofc.my.id to get apikey
+}
+
+//language 
+global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
+
+global.BOT_TOKEN = "6767145965:AAF2TykimOiKzdPZxGkuQTtNv-8E7M9MSdM" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "WauWiuBotzོ" //your bot name
+global.OWNER_NAME = "ᴹᴿ᭄ WauWiu-Ownerོ ×፝֟͜×" //your name
+global.OWNER_NUMBER = "6282114367104" //your telegram number
+global.OWNER = ["https://t.me/jollaakkss", "https://t.me/jollaakkss"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
+global.lang = language //don't change
